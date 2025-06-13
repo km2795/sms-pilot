b0c8bf3b062cba28abeba4110443acdcb6e6aa84
km2795 <kunalkm2795@gmail.com>
2025-06-13 08:45:19 +0530

    HashingVectorizer has been updated.
    Based on python's scikit library

--- app/src/main/java/com/example/smspilot/HashingVectorizer.kt
+++ app/src/main/java/com/example/smspilot/HashingVectorizer.kt
@@ -1,16 +1,70 @@
-package com.example.smspilot;
+package com.example.smspilot
 
+import java.util.Arrays
 import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sign
+import kotlin.math.sqrt
 
-class HashingVectorizer(private val nFeatures: Int) {
-    fun transform(text: String): DoubleArray {
-        var vector = DoubleArray(nFeatures) { 0.0 }
-        var tokens = text.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        for (token in tokens) {
-            val hash = abs(token.hashCode())
-            val idx = hash % nFeatures
-            vector[idx] += 1.0
+/**
+ * Hashing Vectorizer
+ * Based on Python's Scikit Library
+ */
+class HashingVectorizer(private val nFeatures: Int = 2.0.pow(8.0).toInt(),
+                        private val norm: String = "l2",
+                        private val binary: Boolean = false,
+                        private val alternateSign: Boolean = true
+    ) {
+    var input = ""
+    var encoding = "UTF-8"
+    var decodeError = "strict"
+    var stripAccents: String? = null
+    var lowercase = true
+    var tokenPattern = "\\b\\w\\w+\\b"
+    var ngramRange = intArrayOf(1,1)
+    var analyzer = "word"
+
+    fun fit(X: List<String>): DoubleArray {
+        return fitTransform(X)
+    }
+
+    fun fitTransform(X: List<String>): DoubleArray {
+        return transform(X)
+    }
+
+    /**
+     * Transform text data into vectorized format.
+     */
+    fun transform(X: List<String>): DoubleArray {
+        require(!(X == null || X.isEmpty())) { "Iterable over raw text documents expected." }
+
+        val vectorizedData = DoubleArray(nFeatures) // 1D array
+
+        for (doc in X) {
+            val hash = abs((doc.hashCode() % nFeatures).toDouble()).toInt()
+            val value =
+                (if (alternateSign) sign(hash.toDouble()) * abs(hash.toDouble())
+                else abs(hash.toDouble()).toFloat()).toDouble()
+            vectorizedData[hash] += if (binary) 1.0 else value // Accumulate feature values
         }
-        return vector
+
+        // Apply normalization (l1 or l2)
+        if ("l1" == norm) {
+            val sum = Arrays.stream(vectorizedData).sum()
+            if (sum != 0.0) {
+                for (j in 0..<nFeatures) {
+                    vectorizedData[j] /= sum
+                }
+            }
+        } else if ("l2" == norm) {
+            val sumSquared = Arrays.stream(vectorizedData).map { v: Double -> v * v }.sum()
+            if (sumSquared != 0.0) {
+                for (j in 0..<nFeatures) {
+                    vectorizedData[j] /= sqrt(sumSquared)
+                }
+            }
+        }
+
+        return vectorizedData
     }
 }
