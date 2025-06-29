4eeae72307ee0fa614c4226e3462573638926a34
km2795 <kunalkm2795@gmail.com>
2025-06-29 18:31:08 +0530

    Refactor
    ViewModel for each screen (or activity)

---
+++ app/src/main/java/sr79/works/smspilot/LandingPageViewModel.kt
@@ -0,0 +1,63 @@
+package sr79.works.smspilot
+
+import android.app.Application
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.provider.Telephony
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class LandingPageViewModel(private val application: Application): AndroidViewModel(application) {
+
+  // Message list.
+  val _messageList = MutableStateFlow<List<Message>>(emptyList())
+  private val contentResolver = application.contentResolver
+
+  // For the content change. (For updates in the SMSs content provider).
+  private val messageObserver = object: ContentObserver(Handler(Looper.getMainLooper())) {
+    override fun onChange(selfChange: Boolean, uri: Uri?) {
+      super.onChange(selfChange, uri)
+
+      // Update happens. Refresh the list.
+      refreshSmsMessages()
+    }
+  }
+
+  init {
+    contentResolver.registerContentObserver(
+      Telephony.Sms.CONTENT_URI,
+      true,
+      messageObserver
+    )
+  }
+
+  /**
+   * For initial loading of the message list.
+   */
+  fun loadSmsMessages() {
+    viewModelScope.launch {
+      _messageList.value = SmsPilot().getSmsList(contentResolver)
+    }
+  }
+
+  /**
+   * To refresh the message list with changes.
+   */
+  fun refreshSmsMessages() {
+    // Should fetch and check for updates.
+    viewModelScope.launch {
+      _messageList.value = SmsPilot().getSmsList(contentResolver)
+    }
+  }
+
+  /**
+   * To clear the message list.
+   */
+  fun clearSmsMessages() {
+    _messageList.value = emptyList()
+  }
+}
