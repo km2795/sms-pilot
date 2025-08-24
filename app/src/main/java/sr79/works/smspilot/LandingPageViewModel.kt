bd703c460d0a663790350a508455254f8897d518
km2795 <kunalkm2795@gmail.com>
2025-08-24 12:14:21 +0530

    Update [Major]
    1. Change the UI updating modules.
    2. List refreshing module has been updated
    to include all around changes.
    3. runPredictor module has been updated
    to accommodate for different scenarios (
    whether for only few messages or complete list).
    4. Removed unnecessary signatures from
    methods.
    5. Streamlined the whole viewModel.

--- app/src/main/java/sr79/works/smspilot/LandingPageViewModel.kt
+++ app/src/main/java/sr79/works/smspilot/LandingPageViewModel.kt
@@ -8,7 +8,6 @@ import android.net.Uri
 import android.os.Handler
 import android.os.Looper
 import android.provider.Telephony
-import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
@@ -27,31 +26,48 @@ import java.nio.MappedByteBuffer
 
 class LandingPageViewModel(
   private val application: Application,
-  private val dataStore: DataStore,
-  private val detector: MappedByteBuffer?
+  private val dataStore: DataStore
 ): ViewModelProvider.Factory, AndroidViewModel(application) {
 
-  /*
-   * Private data objects for the view model.
+  /**
+   * Holds the list of SMS threads to be displayed in the UI.
+   * Returns a list of [DisplayThread] objects.
    */
-  private val _threadList = MutableStateFlow<List<Thread>>(emptyList())
   private val _displayThreads = MutableStateFlow<List<DisplayThread>>(emptyList())
-  private val _messageList = MutableStateFlow<List<Message>>(emptyList())
 
-  // Stores the messages.
+  /**
+   * A StateFlow that emits the list of displayable SMS threads.
+   * This property provides an observable stream of `List<DisplayThread>` objects.
+   */
+  val displayThreads: StateFlow<List<DisplayThread>> = _displayThreads.asStateFlow()
+
+  /**
+   * Stores all loaded messages, indexed by their unique ID.
+   */
   private var messageIndex: MutableMap<Long, Message> = mutableMapOf()
 
-  /*
-   * Public modes of the view model's data objects.
+  /**
+   * For controlling visibility of the permission button.
    */
-  val threadList: StateFlow<List<Thread>> = _threadList.asStateFlow()
-  val displayThreads: StateFlow<List<DisplayThread>> = _displayThreads.asStateFlow()
-  val messageList: StateFlow<List<Message>> = _messageList
-
-  // For controlling visibility of the permission button.
   private val _showPermissionButton = MutableStateFlow(true)
+
+  /**
+   * A StateFlow that indicates whether the permission button should be shown.
+   */
   val showPermissionButton: StateFlow<Boolean> = _showPermissionButton.asStateFlow()
 
+  /**
+   * Updates the visibility of the permission button.
+   *
+   * If `show` is true, the permission button will be displayed,
+   * and any existing SMS messages in the DataStore will be cleared.
+   * This is typically done when the app needs to re-request SMS permission.
+   *
+   * If `show` is false, the permission button will be hidden.
+   * This is usually done after the permission has been granted.
+   *
+   * @param show A boolean indicating whether to show (true) or hide (false) the permission button.
+   */
   fun updateShowPermissionButton(show: Boolean) {
     _showPermissionButton.value = show
     if (show) {
@@ -59,25 +75,34 @@ class LandingPageViewModel(
     }
   }
 
-  // Handle for content resolver. (Used specifically for content observer).
+  /**
+   * Handle for content resolver. (Used specifically for content observer).
+   */
   private val contentResolver = application.contentResolver
 
-  // For the content change. (For updates in the SMSs content provider).
+  /**
+   * Observes changes in the SMS content provider.
+   */
   private val messageObserver = object: ContentObserver(Handler(Looper.getMainLooper())) {
     override fun onChange(selfChange: Boolean, uri: Uri?) {
       super.onChange(selfChange, uri)
 
       // Update happens. Refresh the list.
-      refreshSmsMessages()
+      refreshMessageList()
     }
   }
 
+  /**
+   * Register an observer for the SMS content provider,
+   * and kick start the functionality of the app.
+   */
   init {
     contentResolver.registerContentObserver(
       Telephony.Sms.CONTENT_URI,
       true,
       messageObserver
     )
+    // Where the App starts it's functionality.
     load()
   }
 
@@ -162,6 +187,7 @@ class LandingPageViewModel(
 
         // Load the Threads.
         _showPermissionButton.value = false
+        loadMessageIndex()
         loadThreads()
       } else {
 
@@ -176,44 +202,87 @@ class LandingPageViewModel(
     return dataStore.getSmsReadPermission(application)
   }
 
-  private fun loadMessageIndex() {
-    var messageList = dataStore.loadMessageList() ?: AppHandler.getMessageList(dataStore, application.contentResolver)
-    for (message in messageList) {
+  /**
+   * Loads messages from the DataStore into the `messageIndex`.
+   *
+   * This function retrieves the list of messages stored in the DataStore.
+   * If the list is not null, it iterates through each message and adds it
+   * to the `messageIndex` map, using the message's ID as the key.
+   *
+   * @return The number of messages loaded into the `messageIndex`.
+   *         Returns 0 if no messages were loaded (e.g., if the DataStore
+   *         returned a null list).
+   */
+  private fun loadMessageIndex(): Int {
+    var messageList = dataStore.loadMessageList() ?: return 0
+
+    messageList.forEach { message ->
       messageIndex[message.getId()] = message
     }
+    return messageIndex.size
   }
 
   /**
-   * Load the Threads.
+   * Return the list of message Ids that are not added to the list.
+   */
+  private fun getUniqueMessages(): List<Message> {
+
+    var uniqueMessages: MutableList<Message> = mutableListOf()
+
+    /*
+     * Fetch messages from content provider
+     * using 'AppHandler.getMessageList()'
+     * and then for each item, check in the
+     * 'messageIndex' for unique message.
+     */
+    AppHandler.getMessageList(application.contentResolver).forEach { item->
+      if (messageIndex[item.getId()] == null) {
+        messageIndex[item.getId()] = item
+        dataStore.storeMessage(item)
+        uniqueMessages.add(item)
+      }
+    }
+
+    return uniqueMessages
+  }
+
+  /**
+   * Loads the SMS threads and updates the UI.
+   *
+   * It retrieves the current list of messages from `messageIndex`,
+   * groups them into threads using `AppHandler.getThreadList`,
+   * and then converts these threads into a displayable format
+   * using `AppHandler.getDisplayThreads`. The resulting list of
+   * `DisplayThread` objects is then assigned to `_displayThreads`,
+   * which triggers an update in the UI observing this StateFlow.
    */
   private fun loadThreads() {
-    viewModelScope.launch(Dispatchers.IO) {
-      loadMessageIndex()
-      val threads = AppHandler.getThreadList(messageIndex.values.toList() as MutableList<Message>)
-      _threadList.value = threads
-      _displayThreads.value = AppHandler.getDisplayThreads(threads)
+    viewModelScope.launch(Dispatchers.Default) {
+      _displayThreads.value = AppHandler.getDisplayThreads(
+        AppHandler.getThreadList(
+          messageIndex.values.toList() as List<Message>
+        )
+      )
     }
   }
 
   /**
-   * To refresh the message list with changes.
-   * Update method is very trivial, and won't
-   * work, when number of messages deleted and
-   * added are same.
+   * Refreshes the message list if there are any changes.
+   *
+   * This function is triggered when a change is detected
+   * in the SMS content provider. It checks for a difference
+   * in the number of messages between the current `messageIndex`
+   * and the messages in the content resolver. If a difference
+   * is found, it reloads the threads to update the UI.
    */
-  fun refreshSmsMessages() {
-    // Should fetch and check for updates.
+  fun refreshMessageList() {
     viewModelScope.launch {
-      var previousSize = _messageList.value.size
-      _messageList.value = AppHandler.getMessageList(dataStore, application.contentResolver)
-      var newSize = _messageList.value.size
+      var uniqueMessages = getUniqueMessages()
 
-      // Check for size difference.
-      // This is very trivial and
-      // not a good solution.
-      if (previousSize != newSize) {
-        loadThreads()
-      }
+      // Run the predictor, only if there are new messages.
+      if (uniqueMessages.isNotEmpty())
+        runPredictor(uniqueMessages)
+      loadThreads()
     }
   }
 
@@ -221,20 +290,35 @@ class LandingPageViewModel(
    * To clear the message list.
    */
   private fun clearSmsMessages() {
-    _messageList.value = emptyList()
+    messageIndex.clear()
     dataStore.clearTable()
   }
 
-  suspend fun runPredictor() {
+  /**
+   * Runs the spam prediction API for a list of messages.
+   *
+   * This function iterates through the provided list of messages. If a message's
+   * spam status has not been previously determined (i.e., `getSpamOrNot()` returns null),
+   * it calls the `predictorApi` to get a spam verdict. If a verdict is received,
+   * the message's spam status is updated, and the message is updated in the DataStore.
+   *
+   * If the `API_URL` is empty, this function does nothing.
+   * If the `messages` parameter is null, it defaults to processing all messages
+   * currently in the `messageIndex`.
+   *
+   * @param messages A list of [Message] objects to be processed. If null, all messages
+   *                 in `messageIndex` will be processed.
+   */
+  fun runPredictor(messages: List<Message>?) {
     viewModelScope.launch {
       if (API_URL != "") {
 
         // Spam API URL.
         val apiUrl = URL(API_URL)
 
-        var messageList = AppHandler.getMessageList(dataStore, application.contentResolver)
+        var messageList = messages ?: messageIndex.values
         for (message in messageList) {
-          // Delay for 10 milliseconds.
+          // Delay.
           delay(5L)
 
           // Check if the spam status is set or not.
@@ -253,23 +337,29 @@ class LandingPageViewModel(
           }
         }
 
+        // Update the UI.
         loadThreads()
-      } else {
-        Log.d("runPredictor", "Aborting, No API URL setup.")
       }
     }
   }
 
-  // Factory for our LandingPageViewModel.
+  /**
+   * Factory class for creating instances of [LandingPageViewModel].
+   *
+   * This factory is responsible for providing the necessary dependencies
+   * to the [LandingPageViewModel] during its construction.
+   *
+   * @property application The application context.
+   * @property dataStore An instance of [DataStore] for accessing persistent data.
+   */
   class LandingPageViewModelFactory(
     private val application: Application,
-    private val dataStore: DataStore,
-    private val detector: MappedByteBuffer?
+    private val dataStore: DataStore
   ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
       if (modelClass.isAssignableFrom(LandingPageViewModel::class.java)) {
-        return LandingPageViewModel(application, dataStore, detector) as T
+        return LandingPageViewModel(application, dataStore) as T
       }
       throw IllegalArgumentException("Unknown ViewModel class")
     }
